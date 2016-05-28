2db1f994f884460a6e207d235a3f3352ecac686c
Adam Benda <adam@adasek.cz>
2016-05-28 11:45:50 +0200

    Firefox fix of btoa

--- public_html/svg-render.js
+++ public_html/svg-render.js
@@ -374,7 +374,7 @@ SVGRender.prototype.renderNextFrame = function () {
     }.bind(this);
 
     this.svgImage.src = "data:image/svg+xml;base64," + btoa("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n\
-        <!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">" + svgString);
+        <!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">" + unescape(encodeURIComponent(svgString)));
 };
 
 /**
